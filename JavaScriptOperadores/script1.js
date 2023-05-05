20b78220c4e940364ac6834bf34b1eeffa5857e9
nathantsdias <nathantsdias@gmail.com>
2023-05-04 21:56:09 -0300

    04.05

---
+++ JavaScriptOperadores/script1.js
@@ -0,0 +1,9 @@
+//script de valor inteiro
+let numero1;
+let numero2;
+let resposta;
+numero1 = prompt('Digite um valor:', 'Digite um número');
+numero2 = prompt('Digite outro valor:', 'Digite um número');
+//realiza a soma de dois nº inteiros
+resposta = parseInt(numero1) + parseInt(numero2);
+document.write('O resultado da Soma é ', resposta);
